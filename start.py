331fe21b1661f0fe906477ab265fca440d1dd192
danil <danil.mirtalibov.27@mail.ru>
2020-07-24 20:42:49 +0300

    Added new file requirements.txt where all requirements for my project are. Created files about.html, all_posts.html and new_post.html. Added routes for these html pages in start.py . Updated base.html file. Now it has a header menu. Next step will be adding a database to my project. Oh yes, f*ck social net. I'm gonna make a blog about unies now. The name of this blog will be UniBlog for now.

--- start.py
+++ start.py
@@ -3,16 +3,25 @@ from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
 
-
+@app.route('/')
 @app.route('/index')
 def index():
           return render_template('index.html')
 
 
-@app.route('/')
-@app.route('/main')
-def main():
-          return "Main"
+@app.route('/about')
+def about():
+          return render_template('about.html')
+
+
+@app.route('/new_post')
+def new_post():
+          return render_template('new_post.html')
+
+
+@app.route('/all_posts')
+def all_posts():
+          return render_template('all_posts.html')
 
 
 if __name__=="__main__":
