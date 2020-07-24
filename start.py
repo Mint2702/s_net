dbfbb979316bad5892ede0928a0c6bcb57a10ba6
danil <danil.mirtalibov.27@mail.ru>
2020-07-24 19:31:50 +0300

    Created templates/ folder to contain .html files. Created a base.html file as a base file for all my future .html files. Created index.html file as the main pafe of the site. Started using url_for functions.

--- start.py
+++ start.py
@@ -1,12 +1,18 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
 
 
 app=Flask(__name__)
 
-@app.route('/')
+
 @app.route('/index')
 def index():
-          return 'Hello World!'
+          return render_template('index.html')
+
+
+@app.route('/')
+@app.route('/main')
+def main():
+          return "Main"
 
 
 if __name__=="__main__":
